6de3e4b5e4ee249ed2b01bbcc5d02cb73fd62a5a
Leo Arruda <HiLeoArruda@gmail.com>
2025-07-14 16:56:22 -0400

    add Container react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hello World!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos
-        voluptate voluptates ipsum exercitationem, aspernatur, qui quas numquam
-        commodi perferendis eligendi facere aut saepe, laborum incidunt dolorem
-        nemo! Eaque, reprehenderit.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
