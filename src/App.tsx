a5cbdd95cd1d34d3711a389dc979808191d1afe8
Leo Arruda <HiLeoArruda@gmail.com>
2025-07-14 15:51:38 -0400

    add style variables & CSS reset

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Heading } from './components/Heading';
 
 export function App() {
   return (
-    <div>
+    <>
       <Heading />
+      <Heading attr={123} attr2='String'>
+        Hello World!
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quos
         voluptate voluptates ipsum exercitationem, aspernatur, qui quas numquam
         commodi perferendis eligendi facere aut saepe, laborum incidunt dolorem
         nemo! Eaque, reprehenderit.
       </p>
-    </div>
+    </>
   );
 }
