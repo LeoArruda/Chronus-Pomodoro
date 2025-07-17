67c185cf5deb8f0fa854460cc10340fd76afc101
Leo Arruda <HiLeoArruda@gmail.com>
2025-07-16 21:58:01 -0400

    defining application's models TaskModel and TaskStateModel

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,10 @@
+import type { TaskStateModel } from './TaskStateModel';
+
+export type TaskModel = {
+  name: string;
+  duration: number;
+  startDate: number;
+  completeDate: number | null; // when the countdown ends up
+  interruptDate: number | null; // when the task is interrupted
+  type: keyof TaskStateModel['config'];
+};
