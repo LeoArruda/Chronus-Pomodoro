e40bd5bab1ef54df932c8a1ea685684142e17d85
Leo Arruda <HiLeoArruda@gmail.com>
2025-07-16 21:34:46 -0400

    reorg folders, add page templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,8 @@
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { Countdown } from './components/Countdown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
-import { useState } from 'react';
+import { Home } from './pages/Home';
 
 export function App() {
-  const [numbers, setNumbers] = useState(0);
-
-  function handleClick() {
-    setNumbers(prevState => prevState + 1);
-    //setNumbers(1);
-  }
-  return (
-    <>
-      <Heading>
-        Number: <span id='numbers'>{numbers}</span>
-      </Heading>
-      <button onClick={handleClick}>Increment</button>
-
-      <Container>
-        <Heading>
-          <Logo />
-        </Heading>
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <Countdown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText={numbers.toString()}
-              id='meuInput'
-              type='text'
-              placeholder='Type anything'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
