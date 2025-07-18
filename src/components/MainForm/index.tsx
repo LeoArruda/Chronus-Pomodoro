e2a9cb3f0d146979c3a386e5f801b40082c49a32
Leo Arruda <HiLeoArruda@gmail.com>
2025-07-17 23:34:45 -0400

    intercepting the component changing

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,21 +2,32 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('WORKED !', taskName);
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
           type='text'
           placeholder='Digite algo'
+          value={taskName}
+          onChange={event => setTaskName(event.target.value)}
         />
       </div>
 
       <div className='formRow'>
-        <p>Próximo intervalo é de 25min</p>
+        <p>Next Interval of 25min</p>
       </div>
 
       <div className='formRow'>
