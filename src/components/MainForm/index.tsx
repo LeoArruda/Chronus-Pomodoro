dd6b825fff69f7e4f4ccbfd6c7f71699a6da98a4
Leo Arruda <HiLeoArruda@gmail.com>
2025-07-21 15:19:54 -0400

    adding react toastify

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/TaskActions';
 import { Tips } from '../Tips';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -21,13 +22,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dismiss();
 
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Please enter a task name');
+      showMessage.warn('Please enter a task name');
       return;
     }
 
@@ -41,9 +43,12 @@ export function MainForm() {
       type: nextCycleType,
     };
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
+    showMessage.success(`Task "${taskName}" started successfully!`);
   }
 
   function handleInterruptTask() {
+    showMessage.dismiss();
+    showMessage.error('Task interrupted');
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
